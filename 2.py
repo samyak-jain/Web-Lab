85bd9b5ebbdd6dc5d605c4a7a7d15ef12d34e32c
Samyak Jain <samtan106@gmail.com>
2018-07-23 10:48:38 +0530

    2nd file

---
+++ 2.py
@@ -0,0 +1,15 @@
+import nltk
+from nltk.corpus import stopwords
+from bs4 import BeautifulSoup
+from collections import Counter
+
+with open('htmlfile.txt') as f:
+    x = f.read()
+    soup = BeautifulSoup(x, 'html.parser')
+    text = soup.get_text()
+    tokens = nltk.word_tokenize(text)
+    stop_words = set(stopwords.words('english'))
+    filtered = [w for w in tokens if w not in stop_words]
+    stemmer = nltk.stemmer.PorterStemmer()
+    stemmed_words = list(map(lambda x: stemmer.stem(x), filtered))
+    print(Counter(stemmed_words))
