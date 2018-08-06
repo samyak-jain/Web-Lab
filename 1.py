e6e4d47f46a923e6c8138ed47d926bbded427109
Samyak Jain <samtan106@gmail.com>
2018-08-06 11:21:41 +0530

    Lab exp 2

--- 1.py
+++ 1.py
@@ -1,7 +1,22 @@
 import nltk
 from collections import Counter
+import sys
+from bs4 import BeautifulSoup as bs
+import requests
 
-with open("textfile.txt") as f:
-    x = f.read()
-    tokens = nltk.word_tokenize(x)
-    print(Counter(tokens))
+def ext(x):
+    soup = bs(x, "lxml")
+    return soup.get_text()
+
+if len(sys.argv) > 1:
+    a = requests.get(sys.argv[1]).text
+    x = ext(a)
+else:
+    with open("textfile.txt") as f:
+        x = f.read()
+
+print(f"The number of words is {len(x.split())}")
+print(f"The number of sentences is " + str(x.count('\n')))
+tokens = nltk.word_tokenize(x)
+print("The count of the tokens is ")
+print(Counter(tokens))
