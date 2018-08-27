576d26e98f1bd10104a3df6aaf1a6b4e832c645f
Samyak Jain <samtan106@gmail.com>
2018-08-27 12:37:15 +0530

    added lab 2

--- 2.py
+++ 2.py
@@ -1,28 +1,116 @@
+import requests
+from nltk.stem import WordNetLemmatizer
 import nltk
 from nltk.corpus import stopwords
 from bs4 import BeautifulSoup
-from collections import Counter
 from nltk.stem.porter import *
 import string
+from nltk.corpus import wordnet as wn
 
-with open('htmlfile.txt') as f:
-    x = f.read()
-    soup = BeautifulSoup(x, 'html.parser')
-    soup.script.decompose()
-    soup.a.decompose()
-    soup.style.decompose()
+def is_noun(tag):
+    return tag in ['NN', 'NNS', 'NNP', 'NNPS']
+
+
+def is_verb(tag):
+    return tag in ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
+
+
+def is_adverb(tag):
+    return tag in ['RB', 'RBR', 'RBS']
+
+
+def is_adjective(tag):
+    return tag in ['JJ', 'JJR', 'JJS']
+
+
+def penn_to_wn(tag):
+    if is_adjective(tag):
+        return wn.ADJ
+    elif is_noun(tag):
+        return wn.NOUN
+    elif is_adverb(tag):
+        return wn.ADV
+    elif is_verb(tag):
+        return wn.VERB
+    return None
+
+def preprocess(text):
+    soup = BeautifulSoup(text, 'html.parser')
+    for i in soup(['a', 'script', 'style']):
+        i.extract()
+
     text = soup.get_text()
-    print("The number of lines is " + str(text.count('\n')))
     translate_table = dict((ord(char), None) for char in string.punctuation)
     text = text.translate(translate_table)
     tokens = nltk.word_tokenize(text)
-    stop_words = set(stopwords.words('english'))
+    stop_words = set(stopwords.words('english') + ['\n'])
     filtered = [w for w in tokens if w not in stop_words]
-    print(f"The number of words after stopword removal is {len(Counter(filtered))}")
-    print("The frequency count after stopword removal is ")
-    print(Counter(filtered))
-    stemmer = PorterStemmer()
-    stemmed_words = list(map(lambda x: stemmer.stem(x), filtered))
-    print(f"\n\nThe number of words after stemming is {len(Counter(stemmed_words))}")
-    print("The frequency count after stemming is ")
-    print(Counter(stemmed_words))
+    wordnet_lemmatizer = WordNetLemmatizer()
+    pos = dict(nltk.pos_tag(tokens))
+    lem_words = [wordnet_lemmatizer.lemmatize(w, pos=penn_to_wn(pos.get(w))) if penn_to_wn(pos.get(w)) is not None else wordnet_lemmatizer.lemmatize(w) for w in filtered]
+    words = set(lem_words)
+    return words
+
+
+if 0:
+    print("Enter 4 URLs")
+    sites = [input(str(i) + ": ") for i in range(4)]
+else:
+    # sites = [
+    #  https://en.wikipedia.org/wiki/Web_mining ,  # 'http://www.vit.ac.in/',
+    #     'https://www.dreamhost.com',
+    #     'https://en.wikipedia.org/wiki/U.S._Snowboarding_Grand_Prix',
+    #     'https://en.wikipedia.org/wiki/2002_FIU_Golden_Panthers_football_team'
+    # ]
+    sites = [
+        'https://en.wikipedia.org/wiki/Web_mining',
+        'https://www.quora.com/What-is-web-mining',
+        'https://www.techopedia.com/definition/15634/web-mining',
+        'https://www.educba.com/data-mining-vs-web-mining/'
+    ]
+
+htmls = [requests.get(i).text for i in sites]
+
+
+indices = []
+for count,i in enumerate(htmls):
+    soup = BeautifulSoup(i, 'html.parser')
+    for i in soup(['a', 'script', 'style']):
+        i.extract()
+
+    text = soup.get_text()
+    translate_table = dict((ord(char), None) for char in string.punctuation)
+    text = text.translate(translate_table)
+    tokens = nltk.word_tokenize(text)
+    stop_words = set(stopwords.words('english') + ['\n'])
+    filtered = [w for w in tokens if w not in stop_words]
+    wordnet_lemmatizer = WordNetLemmatizer()
+    pos = dict(nltk.pos_tag(tokens))
+    lem_words = [wordnet_lemmatizer.lemmatize(w, pos=penn_to_wn(pos.get(w))) if penn_to_wn(pos.get(w)) is not None else wordnet_lemmatizer.lemmatize(w) for w in filtered]
+    for i in range(4):
+        with open(str(i) + '.txt', 'w', encoding='utf-8') as f:
+            f.write(text)
+    words = set(lem_words)
+    index = {}
+    for i in words:
+        index[i.encode('utf-8')] = [c for c,j in enumerate(lem_words) if j==i]
+
+    print(index)
+    indices.append(index)
+
+    with open(str(count) + '_index.txt', 'w') as g:
+        g.write(str(index))
+
+
+terms = set()
+term_index = {}
+for i in htmls:
+    terms = terms | preprocess(i)
+
+for i in terms:
+    term_index[i] = [{c: indices[c].get(i.encode())} for c,j in enumerate(htmls) if indices[c].get(i.encode()) is not None]
+
+print(term_index)
+
+with open("term_index.txt", "w") as g:
+    g.write(str(term_index))
--- 3.py
+++ 3.py
@@ -1,22 +1,98 @@
+from nltk.stem import WordNetLemmatizer
 import nltk
 from nltk.corpus import stopwords
-from bs4 import BeautifulSoup
-from collections import Counter
-from nltk.stem.porter import *
 import string
+from nltk.corpus import wordnet as wn
+import ast
+import math
+import numpy as np
+from scipy.spatial.distance import cdist
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-with open('htmlfile.txt') as f:
-    x = f.read()
-    soup = BeautifulSoup(x, 'html.parser')
-    soup.script.decompose()
-    soup.a.decompose()
-    soup.style.decompose()
-    text = soup.get_text()
+def is_noun(tag):
+    return tag in ['NN', 'NNS', 'NNP', 'NNPS']
+
+
+def is_verb(tag):
+    return tag in ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
+
+
+def is_adverb(tag):
+    return tag in ['RB', 'RBR', 'RBS']
+
+
+def is_adjective(tag):
+    return tag in ['JJ', 'JJR', 'JJS']
+
+
+def penn_to_wn(tag):
+    if is_adjective(tag):
+        return wn.ADJ
+    elif is_noun(tag):
+        return wn.NOUN
+    elif is_adverb(tag):
+        return wn.ADV
+    elif is_verb(tag):
+        return wn.VERB
+    return None
+
+def preprocess(text):
     translate_table = dict((ord(char), None) for char in string.punctuation)
     text = text.translate(translate_table)
     tokens = nltk.word_tokenize(text)
-    stop_words = set(stopwords.words('english'))
-    stop_words.add('The')
+    stop_words = set(stopwords.words('english') + ['\n'])
     filtered = [w for w in tokens if w not in stop_words]
-    print(len(filtered))
-    print(Counter(filtered))
+    wordnet_lemmatizer = WordNetLemmatizer()
+    pos = dict(nltk.pos_tag(tokens))
+    lem_words = [wordnet_lemmatizer.lemmatize(w, pos=penn_to_wn(pos.get(w))) if penn_to_wn(pos.get(w)) is not None else wordnet_lemmatizer.lemmatize(w) for w in filtered]
+    words = set(lem_words)
+    index = {}
+    for i in words:
+        index[i.encode('utf-8')] = [c for c,j in enumerate(lem_words) if j==i]
+
+    return index
+
+def get_cosine(vec1, vec2):
+     intersection = set(vec1.keys()) & set(vec2.keys())
+     numerator = sum([len(vec1[x]) * len(vec2[x]) for x in intersection])
+
+     sum1 = sum([len(vec1[x])**2 for x in vec1.keys()])
+     sum2 = sum([len(vec2[x])**2 for x in vec2.keys()])
+     denominator = math.sqrt(sum1) * math.sqrt(sum2)
+     print(vec2.keys())
+
+     if not denominator:
+        return 0.0
+     else:
+        return float(numerator) / denominator
+
+def euc(vec1, vec2):
+    union = set(vec1.keys()) | set(vec2.keys())
+    numerator = sum([(len(vec1.get(x, [])) - len(vec2.get(x, [])))**2 for x in union])**(1/2)
+    denominator = len(vec1.keys()) * len(vec2.keys())
+
+    if not denominator:
+        return 0.0
+    else:
+        return numerator / denominator
+
+vectorizer = TfidfVectorizer()
+indices = []
+for i in range(4):
+    with open(str(i) + '_index.txt') as f:
+        indices.append(f.read())
+
+query = input("Enter Query: ")
+cosines = [get_cosine(preprocess(query), ast.literal_eval(i)) for i in indices]
+print(cosines)
+
+print("The ranking based on cosine similarity is")
+for i in np.asarray(cosines).argsort()[:-len(cosines)-1:-1]:
+    print(i+1)
+
+eucs = [euc(preprocess(query), ast.literal_eval(i)) for i in indices]
+print(eucs)
+
+print("The ranking based on Euclidean Distance is")
+for i in np.asarray(eucs).argsort()[:-len(eucs)-1:-1][::-1]:
+    print(i+1)
